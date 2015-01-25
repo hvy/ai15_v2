b5ad1699c10688f46bfacd0cd117161f19d8ae36
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2015-01-25 11:38:53 +0100

    Added a button that onClick should create game obstacles for the discrete case

---
+++ a1/Assets/StageGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageGenerator : MonoBehaviour {
+
+	// TODO
+	public void createDiscreteStage() {
+
+		Debug.Log ("Create discrete stage");
+
+		double[] posX = { 100.0, 200.2 };
+		double[] posY = { 100.0, 205.0 };
+
+		int points = posX.Length;
+
+		for (int i = 0; i < points; i++) {
+			double x = posX[i];
+			double y = posY[i];
+
+			// Create a wall from x to y
+		}
+	}
+
+	// TODO
+	public void createContinuousStage() {
+
+	}
+}
