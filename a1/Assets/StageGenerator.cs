5a1e76314aed7eeb376122d8897409dbb044c917
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2015-01-26 20:12:06 +0100

    Cleaned up some code in the StageGenerator

--- a1/Assets/StageGenerator.cs
+++ a1/Assets/StageGenerator.cs
@@ -24,50 +24,27 @@ public class StageGenerator : MonoBehaviour {
 		}
 	};
 
-	public void createEmptyStage() {
+	public void createSimpleStage() {
 
 		clearStage ();
+		createEmptyStage ();
 
-		// Create the stage
-		stage = GameObject.CreatePrimitive (PrimitiveType.Cube);
-		stage.transform.position = new Vector3 (totWidth / 2, 0, totHeight / 2);
-		stage.transform.localScale = new Vector3 (totWidth, 1, totHeight);
-
-		// Create the start and the end points
-		GameObject start = GameObject.CreatePrimitive (PrimitiveType.Cube);
-		start.transform.localScale = new Vector3 (5, 0, 5);
-		start.transform.position = new Vector3 (10, 10, 10);
-		start.renderer.material.color = startColor;
-		start.transform.parent = stage.transform;
-
-		GameObject end = GameObject.CreatePrimitive (PrimitiveType.Cube);
-		end.transform.localScale = new Vector3 (5, 0, 5);
-		end.transform.position = new Vector3 (90, 10, 90);
-		end.renderer.material.color = endColor;
-		end.transform.parent = stage.transform;
+		setStart (new Vector2 (10, 10));
+		setEnd (new Vector2 (90, 90));
 	}
 
 	public void createDiscreteStage() {
 
 		clearStage ();
+		createEmptyStage ();
 
-		startPos = new Vector2 (1, 1);
-		endPos = new Vector2 (19, 19);
+		Vector2 startTile = new Vector2 (1, 1);
+		Vector2 endTile = new Vector2 (19, 19);
 
 		int numTilessWidth = 20;
 		int numTilesHeight = 20;
 		int tileWidth = totWidth / numTilessWidth;
 		int tileHeight = totHeight / numTilesHeight;
-
-		// Create the stage
-		stage = GameObject.CreatePrimitive (PrimitiveType.Cube);
-		//float actualWidth = numTilessWidth * tileWidth;
-		//float actualHeight = numTilesHeight * tileHeight;
-		//stage.transform.position = new Vector3 (actualWidth / 2, 0, actualHeight / 2);
-		//stage.transform.localScale = new Vector3 (actualWidth, 1, actualHeight);
-
-		stage.transform.position = new Vector3 (totWidth / 2, 0, totHeight / 2);
-		stage.transform.localScale = new Vector3 (totWidth, 1, totHeight);
 
 		// Create the walls
 		bool[,] walkable = new bool[20, 20] {
@@ -95,23 +72,15 @@ public class StageGenerator : MonoBehaviour {
 
 		for (int i = 0; i < walkable.GetLength(0); i++) {
 			for (int j = 0; j < walkable.GetLength(1); j++) {
-				if (i == startPos.x && j == startPos.y) {
+				if (i == startTile.x && j == startTile.y) {
 
-					// Create the start point
-					GameObject start = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					start.transform.localScale = new Vector3(tileWidth / 2, 0.0f, tileHeight / 2);
-					start.transform.position = new Vector3((i * tileWidth) + (tileWidth / 2),  10.0f, (j * tileHeight) + (tileHeight / 2));
-					start.renderer.material.color = startColor;
-					start.transform.parent = stage.transform;
+					// Set start
+					setStart (new Vector2((i * tileWidth) + (tileWidth / 2),(j * tileHeight) + (tileHeight / 2)));
 
-				} else if (i == endPos.x && j == endPos.y) {
+				} else if (i == endTile.x && j == endTile.y) {
 
-					// Create the end point
-					GameObject end = GameObject.CreatePrimitive(PrimitiveType.Cube);
-					end.transform.localScale = new Vector3(tileWidth / 2, 0.0f, tileHeight / 2);
-					end.transform.position = new Vector3((i * tileWidth) + (tileWidth / 2),  10.0f, (j * tileHeight) + (tileHeight / 2));
-					end.renderer.material.color = endColor;
-					end.transform.parent = stage.transform;
+					// Set end
+					setEnd (new Vector2((i * tileWidth) + (tileWidth / 2), (j * tileHeight) + (tileHeight / 2)));
 
 				} else if (!walkable[i, j]) { 
 
@@ -129,13 +98,47 @@ public class StageGenerator : MonoBehaviour {
 	public void createContinuousStage() {
 
 		clearStage ();
-		
-		startPos = new Vector2 (20, 10);
-		endPos = new Vector2 (80, 90);
+		createEmptyStage ();
 
-		// Create the stage
-		
+		setStart (new Vector2 (20, 10));
+		setEnd (new Vector2 (80, 90));
+
 		// Create the walls
+		stage.AddComponent("MeshFilter");
+		stage.AddComponent("MeshRenderer");
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.Clear();
+		mesh.vertices = new Vector3[] {new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0)};
+		mesh.uv = new Vector2[] {new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1)};
+		mesh.triangles = new int[] {0, 1, 2};
+	}
+
+	private void createEmptyStage() {
+		stage = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		stage.transform.position = new Vector3 (totWidth / 2, 0, totHeight / 2);
+		stage.transform.localScale = new Vector3 (totWidth, 1, totHeight);
+	}
+
+	private void setStart(Vector2 pos) {
+
+		startPos = pos;
+
+		GameObject start = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		start.transform.localScale = new Vector3 (2.5f, 0, 2.5f);
+		start.transform.position = new Vector3 (pos.x, 10.0f, pos.y);
+		start.renderer.material.color = startColor;
+		start.transform.parent = stage.transform;
+	}
+
+	private void setEnd(Vector2 pos) {
+
+		endPos = pos;
+
+		GameObject end = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		end.transform.localScale = new Vector3 (2.5f, 0, 2.5f);
+		end.transform.position = new Vector3 (pos.x, 10.0f, pos.y);
+		end.renderer.material.color = endColor;
+		end.transform.parent = stage.transform;
 	}
 
 	private void clearStage() {
