c8bc1907e0b1764e4ef5fa7a53c691bb5d69ed8a
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2015-01-28 01:50:48 +0100

    Textures added. Levels are created randomly.

---
+++ a1/Assets/StageManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageManager : MonoBehaviour{
+
+	public GameObject stage;
+
+	// Discrete stage
+	public int numBoxes;
+	public Transform box;
+
+	// Continuous stage
+
+	public void clearStage() {
+
+		int childCount = stage.transform.childCount;
+
+		for (int i = 0; i < childCount; i++) {
+			GameObject child = stage.transform.GetChild(i).gameObject;
+			if (child.name == "Static") {
+				continue; // Do not destroy static objects such as the ground and the walls
+			}		
+			Object.Destroy(child);
+		}
+	}
+
+	// Discrete stage
+	public void createDiscreteStage() {
+		float boxSize = box.transform.localScale.x;
+		float halfBoxSize = boxSize / 2.0f;
+		
+		for (int i = 0; i < numBoxes; i++) {
+			int xBoxNum = Random.Range(0, 20);			
+			int yBoxNum = Random.Range(0, 20);			
+			float x = xBoxNum * boxSize + halfBoxSize;			
+			float y = yBoxNum * boxSize + halfBoxSize;
+			Transform boxTransform = GameObject.Instantiate(box, new Vector3(x, 0, y), Quaternion.identity) as Transform;
+			boxTransform.parent = stage.transform;
+		}
+	}
+}
---
+++ a1/Assets/WaypointRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointRotation : MonoBehaviour {
+
+	public Vector3 RotateAmount;
+	
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(RotateAmount * Time.deltaTime);
+	}
+}
