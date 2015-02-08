10b34eb7af7fe797eb382e7dcb839595194f1201
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2015-02-08 11:01:06 +0100

    Polygonal level properly cleaned up

---
+++ a1/Assets/Scripts/PolygonalLevelParser.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class PolygonalLevelParser {
+		
+	private Vector2 start, goal;
+	private int width, height;
+	private List<Vector2> vertices;
+	
+	public PolygonalLevelParser() {
+		clearParser ();
+	}
+	
+	public void parse(string fileName) {
+		
+		StreamReader sr = new StreamReader(Application.dataPath + "/Levels/" + fileName);
+		
+		string line;
+		string[] splitLine;
+		
+		// Width and height in number of obstacles
+		line = sr.ReadLine();
+		splitLine = line.Split(',');
+		width = int.Parse(splitLine[0]);
+		height = int.Parse(splitLine[1]);
+		
+		// Start and goal
+		line = sr.ReadLine();
+		splitLine = line.Split(',');
+		start = new Vector2(float.Parse(splitLine[0]), float.Parse(splitLine[1]));
+		line = sr.ReadLine();
+		splitLine = line.Split(',');
+		goal = new Vector2(float.Parse(splitLine[0]), float.Parse(splitLine[1]));
+		
+		// Read all obstacle positions
+		while ((line = sr.ReadLine ()) != null) {
+			splitLine = line.Split(',');
+			Vector2 vertexPosition = new Vector2(float.Parse(splitLine[0]), float.Parse(splitLine[1]));
+			vertices.Add (vertexPosition);
+		}
+		
+		sr.Close ();
+	}
+	
+	public Vector2 getStart() {
+		return start;
+	}
+	
+	public Vector2 getGoal() {
+		return goal;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public List<Vector2> getVertices() {
+		return vertices;
+	}
+	
+	public void clearParser ()
+	{
+		start = new Vector2(0, 0);
+		goal = new Vector2(0, 0);
+		vertices = new List<Vector2> ();
+	}
+}
--- a1/Assets/Scripts/StageManager.cs
+++ a1/Assets/Scripts/StageManager.cs
@@ -7,7 +7,7 @@ public class StageManager : MonoBehaviour
 {
 
 		// Input file name
-		public string discreteLevelFileName;
+		public string discreteLevelFileName, polygonalLevelFileName;
 
 		// Objects acting as parent object to instantiated obstacles and waypoint. Used for cleanup
 		public GameObject stage, waypoints;
@@ -70,35 +70,112 @@ public class StageManager : MonoBehaviour
 		public void createContinuousStage ()
 		{
 
-				clearStage ();
-				polygons = new List<Vector2[]> ();
+		clearStage ();
+		polygons = new List<Vector2[]> ();
 
-				// Test obstacle with 3 vertices
-				Vector2[] vertices3 = new Vector2[3];
-				vertices3 [0] = new Vector2 (60, 50);
-				vertices3 [1] = new Vector2 (55, 70);
-				vertices3 [2] = new Vector2 (90, 60);
-				GameObject obstacleA = ObstacleFactory.createPolygonalObstacle (vertices3);
-				
+		PolygonalLevelParser plp = new PolygonalLevelParser ();
+		plp.parse (polygonalLevelFileName);
+		int width = plp.getWidth ();  
+		int height = plp.getHeight ();
+		int numWaypoints = 0;
+		List<Vector2> vertices = plp.getVertices ();
+			
+		Debug.Log ("Length: " + vertices.Count);
 
-				// Test obstacle with 4 vertices
-				Vector2[] vertices4 = new Vector2[4];
-				vertices4 [0] = new Vector2 (20, 0);
-				vertices4 [1] = new Vector2 (20, 75);
-				vertices4 [2] = new Vector2 (30, 75);
-				vertices4 [3] = new Vector2 (30, 0);
-				GameObject obstacleB = ObstacleFactory.createPolygonalObstacle (vertices4);
+		// HARDCODED for this specific input file
+		Vector2[] vertices4_1 = new Vector2[4];
+		vertices4_1 [0] = vertices[3];
+		vertices4_1 [1] = vertices[2];
+		vertices4_1 [2] = vertices[1];
+		vertices4_1 [3] = vertices[0];
+		GameObject obstacle4_1 = ObstacleFactory.createPolygonalObstacle (vertices4_1);
 
-				Vector2[] vertices4_2 = new Vector2[4];
-				vertices4_2 [0] = new Vector2 (40, 20);
-				vertices4_2 [1] = new Vector2 (40, 100);
-				vertices4_2 [2] = new Vector2 (50, 100);
-				vertices4_2 [3] = new Vector2 (50, 20);
-				GameObject obstacleC = ObstacleFactory.createPolygonalObstacle (vertices4_2);
-				
-				polygons.Add (vertices4);
-				polygons.Add (vertices4_2);
-				polygons.Add (vertices3);
+		Vector2[] vertices4_2 = new Vector2[4];
+		vertices4_2 [0] = vertices[10];
+		vertices4_2 [1] = vertices[11];
+		vertices4_2 [2] = vertices[12];
+		vertices4_2 [3] = vertices[13];
+		GameObject obstacle4_2 = ObstacleFactory.createPolygonalObstacle (vertices4_2);
+
+		Vector2[] vertices4_3 = new Vector2[4];
+		vertices4_3 [0] = vertices[14];
+		vertices4_3 [1] = vertices[15];
+		vertices4_3 [2] = vertices[16];
+		vertices4_3 [3] = vertices[17];
+		GameObject obstacle4_3 = ObstacleFactory.createPolygonalObstacle (vertices4_3);
+
+		Vector2[] vertices5_1a = new Vector2[3];
+		Vector2[] vertices5_1b = new Vector2[4];
+		vertices5_1a [0] = vertices[20];
+		vertices5_1a [1] = vertices[21];
+		vertices5_1a [2] = vertices[22];
+		vertices5_1b [0] = vertices[18];
+		vertices5_1b [1] = vertices[19];
+		vertices5_1b [2] = vertices[20];
+		vertices5_1b [3] = vertices[22];
+		GameObject obstacle5_1a = ObstacleFactory.createPolygonalObstacle (vertices5_1a);
+		GameObject obstacle5_1b = ObstacleFactory.createPolygonalObstacle (vertices5_1b);
+
+		Vector2[] vertices6_1a = new Vector2[4];
+		Vector2[] vertices6_1b = new Vector2[4];
+		vertices6_1a [0] = vertices[6];
+		vertices6_1a [1] = vertices[7];
+		vertices6_1a [2] = vertices[8];
+		vertices6_1a [3] = vertices[9];
+		vertices6_1b [0] = vertices[4];
+		vertices6_1b [1] = vertices[5];
+		vertices6_1b [2] = vertices[6];
+		vertices6_1b [3] = vertices[9];
+		GameObject obstacle6_1a = ObstacleFactory.createPolygonalObstacle (vertices6_1a);
+		GameObject obstacle6_1b = ObstacleFactory.createPolygonalObstacle (vertices6_1b);
+
+		obstacle4_1.transform.parent = stage.transform;
+		obstacle4_2.transform.parent = stage.transform;
+		obstacle4_3.transform.parent = stage.transform;
+		obstacle5_1a.transform.parent = stage.transform;
+		obstacle5_1b.transform.parent = stage.transform;
+		obstacle6_1a.transform.parent = stage.transform;
+		obstacle6_1b.transform.parent = stage.transform;
+
+		polygons.Add (vertices4_1);
+		polygons.Add (vertices4_2);
+		polygons.Add (vertices4_3);
+		polygons.Add (vertices5_1a);
+		polygons.Add (vertices5_1b);
+		polygons.Add (vertices6_1a);
+		polygons.Add (vertices6_1b);
+
+			/*
+			clearStage ();
+			polygons = new List<Vector2[]> ();
+
+			// Test obstacle with 3 vertices
+			Vector2[] vertices3 = new Vector2[3];
+			vertices3 [0] = new Vector2 (60, 50);
+			vertices3 [1] = new Vector2 (55, 70);
+			vertices3 [2] = new Vector2 (90, 60);
+			GameObject obstacleA = ObstacleFactory.createPolygonalObstacle (vertices3);
+			
+
+			// Test obstacle with 4 vertices
+			Vector2[] vertices4 = new Vector2[4];
+			vertices4 [0] = new Vector2 (20, 0);
+			vertices4 [1] = new Vector2 (20, 75);
+			vertices4 [2] = new Vector2 (30, 75);
+			vertices4 [3] = new Vector2 (30, 0);
+			GameObject obstacleB = ObstacleFactory.createPolygonalObstacle (vertices4);
+
+			Vector2[] vertices4_2 = new Vector2[4];
+			vertices4_2 [0] = new Vector2 (40, 20);
+			vertices4_2 [1] = new Vector2 (40, 100);
+			vertices4_2 [2] = new Vector2 (50, 100);
+			vertices4_2 [3] = new Vector2 (50, 20);
+			GameObject obstacleC = ObstacleFactory.createPolygonalObstacle (vertices4_2);
+			
+			polygons.Add (vertices4);
+			polygons.Add (vertices4_2);
+			polygons.Add (vertices3);
+			*/
 
 		}
 
