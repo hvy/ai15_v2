a7876bf586f035b95c52f5487d103272b19d0856
Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>
2015-02-25 15:38:15 +0100

    added waypoint factory and a test object

--- a1/Assets/Scripts/Test.cs
+++ a1/Assets/Scripts/Test.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 // Used for debugging modules
 public class Test : MonoBehaviour {
-	
+
 	void Start () {
 
 		// Test stage creation
@@ -11,6 +12,20 @@ public class Test : MonoBehaviour {
 		float width = 100f;
 		float height = 100f;
 		stageFactory.createStage (width, height);	
+
+
+		//WaypointFactory waypointFactory = new WaypointFactory ();
+
+		int numWaypoints = 100;
+		List<GameObject> waypoints = new List<GameObject> ();
+
+		//WaypointFactory waypointFactory = new WaypointFactory ();
+
+		for (int i = 0; i < numWaypoints; i++) {
+			GameObject waypoint = WaypointFactory.createWaypoint();
+			waypoints.Add(waypoint);
+		}
+
 	}
 
 	void Update () {
