48a8210f776c6485f12f6010e5b288ff76357c8b
Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>
2015-02-25 15:01:53 +0100

    added stage factory

---
+++ a1/Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Used for debugging modules
+public class Test : MonoBehaviour {
+	
+	void Start () {
+
+		// Test stage creation
+		StageFactory stageFactory = new StageFactory ();
+		float width = 100f;
+		float height = 100f;
+		stageFactory.createStage (width, height);	
+	}
+
+	void Update () {
+	
+	}
+}
---
+++ a1/Assets/Scripts/ass2t1/StageFactory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageFactory : MonoBehaviour {
+
+	public StageFactory () {
+
+	}
+
+	public GameObject createStage(float width, float height) {
+
+		Debug.Log ("Creating stage W: " + width + " H: " + height);
+
+		GameObject stage = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		stage.transform.position = new Vector3 (width / 2.0f, -0.5f, height / 2.0f);
+		stage.transform.localScale = new Vector3 (width, -1.0f, height);
+		stage.name = "Stage";
+
+		return stage;
+	}
+}
